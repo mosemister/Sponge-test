9bb9c75a8094e205852f1ff63426bc02accb828d
Lukamon24 <86870824+Lukamon24@users.noreply.github.com>
2021-12-04 16:24:20 +0530

    Fly command added!

---
+++ src/main/java/com/github/lukamon24/testsponge/commands/fly.java
@@ -0,0 +1,38 @@
+package com.github.lukamon24.testsponge.commands;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandExecutor;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.exception.CommandException;
+import org.spongepowered.api.command.parameter.CommandContext;
+import org.spongepowered.api.data.Keys;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.gamemode.GameModes;
+
+public class fly implements CommandExecutor {
+
+    @Override
+    public CommandResult execute(CommandContext context) throws CommandException {
+        if(context.subject() instanceof Player) {
+            Player player = (Player) context;
+            if(context.hasPermission("testsponge.fly")) {
+                if(player.get(Keys.GAME_MODE).equals(GameModes.SURVIVAL)) {
+                    player.offer(Keys.CAN_FLY, true);
+                    player.sendMessage(Component.text("Flight enabled!").color(NamedTextColor.AQUA));
+                }
+                else {
+                    player.sendMessage(Component.text("Please check if you are in the right gamemode!").color(NamedTextColor.RED));
+                }
+            }
+            else {
+                player.sendMessage(Component.text("No Permission!").color(NamedTextColor.DARK_RED));
+            }
+        }
+        else {
+            Sponge.server().sendMessage(Component.text("You need to be a player to do this!").color(NamedTextColor.DARK_RED));
+        }
+        return CommandResult.success();
+    }
+}
--- src/main/java/com/github/lukamon24/testsponge/main.java
+++ src/main/java/com/github/lukamon24/testsponge/main.java
@@ -1,22 +1,28 @@
 package com.github.lukamon24.testsponge;
 
 import com.google.inject.Inject;
+import net.kyori.adventure.text.Component;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.api.Server;
+import org.spongepowered.api.command.Command;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.lifecycle.ConstructPluginEvent;
+import org.spongepowered.api.event.lifecycle.RegisterCommandEvent;
 import org.spongepowered.api.event.lifecycle.StartingEngineEvent;
+import org.spongepowered.api.event.lifecycle.StoppingEngineEvent;
 import org.spongepowered.plugin.PluginContainer;
 import org.spongepowered.plugin.builtin.jvm.Plugin;
 
-@Plugin("testsponge")
-public class TestSponge {
+import com.github.lukamon24.testsponge.commands.fly;
 
-    public final PluginContainer container;
-    public final Logger logger;
+@Plugin("testsponge")
+public class main {
+
+    private final PluginContainer container;
+    private final Logger logger;
 
     @Inject
-    TestSponge(final PluginContainer container, final Logger logger) {
+    main(final PluginContainer container, final Logger logger) {
         this.container = container;
         this.logger = logger;
     }
@@ -28,8 +34,21 @@ public class TestSponge {
     }
 
     @Listener
+    public void onRegisterCommands(final RegisterCommandEvent<Command.Parameterized> event) {
+        //Reg cmd
+        event.register(this.container, Command.builder().executor(new fly()).permission("testsponge.fly").shortDescription(Component.text("Just a fly command!")).build(), "fly");
+    }
+
+    @Listener
     public void onServerStarting(final StartingEngineEvent<Server> event) {
         // Any setup per-game instance. This can run multiple times when
         // using the integrated (singleplayer) server.
+        logger.info("Enjoy!");
     }
+
+    @Listener
+    public void onServerStopping(final StoppingEngineEvent<Server> event) {
+        logger.info("Goodbye and thank you!");
+    }
+
 }
